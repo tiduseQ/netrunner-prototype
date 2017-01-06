258ab75500023b219db1cf092413f0959cf547c3
tiduseQ <tiduseq@gmail.com>
2017-01-06 10:37:41 +0100

    added Heap
    added Heap but did not script it

--- Assets/Resources/Cards/001/002/_002_Script.cs
+++ Assets/Resources/Cards/001/002/_002_Script.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class _002_Script : CardEvent {
 
-	public GameObject Obj_Runner;
+	public GameObject Obj_Rig;
+	public GameObject Obj_Hand;
+	public GameObject Obj_Deck;
+
+	void Start () {
+		Obj_Rig = GameObject.Find ("Rig");
+		Obj_Hand = GameObject.Find ("Hand");
+		Obj_Deck = GameObject.Find ("Deck");
+	}
 
 	protected override void PlayEvent() {
 		if (AssessCost ()) {
